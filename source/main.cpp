af4ff8dd38b3123ce52b15462f475b22d4a2a874
aumeinctutw1 <bennoschnabel@MacBook-Air-von-Benno.fritz.box>
2025-03-03 20:21:14 +0100

    training and querying via separate functions

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <filesystem>
 
+/* NN Stuff */
 #include "neuralnetwork.h"
 #include "activations.h"
 #include "vectorops.h"
@@ -94,55 +95,54 @@ int getIndexOfTarget(const std::vector<T> &output) {
     return std::distance(output.begin(), it);
 }
 
+template <typename T>
+void testModel(std::string test_csv, NeuralNetwork<T> &nn) {
+    /* query the model with test data */
+    std::cout << "Querying model with test data" << std::endl;
+    std::vector<std::vector<float>> test_data = readCSV<float>(test_csv);
+    int scoreboard = 0; 
+    for (int i = 0; i < test_data.size(); i++) {
+        std::vector<float> input = getInput<float>(test_data.at(i));
+        std::vector<float> prediction = nn.query(input);
+        std::cout << "Prediction: " << getIndexOfTarget<float>(prediction) << " ";
+        std::cout << "Target: " << test_data.at(i).at(0) << std::endl;
+        if (getIndexOfTarget<float>(prediction) == test_data.at(i).at(0)) {
+            scoreboard++;
+        }
+    }
+
+    /* print the accuracy */
+    std::cout << "Accuracy: " << (scoreboard / (float)test_data.size()) * 100 << "%" << std::endl;
+}
+
+template <typename T>
+void trainModel(std::string training_csv, NeuralNetwork<T> &nn) {
+    /* read training csv */
+    std::vector<std::vector<float>> training_data = readCSV<float>(training_csv);
+    
+    /* train the model */
+    for (int i = 0; i < training_data.size(); i++) {
+        std::vector<float> input = getInput<float>(training_data.at(i));
+        std::vector<float> target = getTargets<float>(training_data.at(i), 10);
+        nn.train(input, target);
+    }
+}
+
+template <typename T>
+void queryModel(NeuralNetwork<T> &nn, std::vector<T> input) {
+    std::cout << "Querying model with input" << std::endl;
+    std::vector<T> output = nn.query(input);
+    std::cout << "Prediction: " << getIndexOfTarget<T>(output) << std::endl;
+}
 
 int main (int argc, const char *argv[]) {
     try {
-        /* get csv path from commandline */
-        if (argc != 3) {
-            std::cerr << "Usage: " << argv[0] << " <training csv file> <test csv file>" << std::endl;
-            throw std::invalid_argument("No csv file provided");
-        }
-
-        std::string training_csv = argv[1];
-        std::string test_csv = argv[2];
-
+        /* create a neural network */
         NeuralNetwork nn = NeuralNetwork<float>({{784, "none"}, {100, "sigmoid"}, {10, "sigmoid"}}, 0.3);
-
         if (std::filesystem::exists("model.txt")) {
-            std::cout << "Loading model from filesystem" << std::endl;
+            std::cout << "Loading model from file" << std::endl;
             nn.loadModel("model.txt");
-        } else {
-            std::cout << "No model found, training new model" << std::endl;
-
-            /* read training csv */
-            std::vector<std::vector<float>> training_data = readCSV<float>(training_csv);
-            
-            /* train the model */
-            for (int i = 0; i < training_data.size(); i++) {
-                std::vector<float> input = getInput<float>(training_data.at(i));
-                std::vector<float> target = getTargets<float>(training_data.at(i), 10);
-                nn.train(input, target);
-            }
         }
-
-        /* query the model with test data */
-        std::cout << "Querying model with test data" << std::endl;
-        std::vector<std::vector<float>> test_data = readCSV<float>(test_csv);
-        int scoreboard = 0; 
-        for (int i = 0; i < test_data.size(); i++) {
-            std::vector<float> input = getInput<float>(test_data.at(i));
-            std::vector<float> prediction = nn.query(input);
-            std::cout << "Prediction: " << getIndexOfTarget<float>(prediction) << " ";
-            std::cout << "Target: " << test_data.at(i).at(0) << std::endl;
-            if (getIndexOfTarget<float>(prediction) == test_data.at(i).at(0)) {
-                scoreboard++;
-            }
-        }
-
-        /* print the accuracy */
-        std::cout << "Accuracy: " << (scoreboard / (float)test_data.size()) * 100 << "%" << std::endl;
-
-        nn.saveModel("model.txt");
     } catch (const std::exception &e) {
         std::cerr << "Exception occurred: " << e.what() << std::endl;
         return 1;
